baf0f5a8aeb9eda8364d6b795a94168ce727ddcf
yasaidori <yasaidori3571@gmail.com>
2025-03-15 20:03:54 +0900

    update for old_1_4_1

---
+++ docs/old/old_1_4_1/custumtable_script.js
@@ -0,0 +1,149 @@
+function createTable(X, DamageStart, BoostStart , A) {
+    
+    X = parseFloat(X);
+    DamageStart = parseFloat(DamageStart);
+    BoostStart = parseFloat(BoostStart);
+    A = parseFloat(A);
+    
+    
+    let damageRange = Array.from({length: 10}, (_, i) => i + DamageStart);
+    let boostRange = Array.from({length: 10}, (_, i) => i + BoostStart);
+    
+    if (DamageStart>0){
+        damageRange.unshift(0);
+        damageRange.pop();
+    }
+    if (BoostStart>0){
+        boostRange.unshift(0);
+        boostRange.pop();
+    }
+    
+    
+    const headerRow = document.getElementById('header-row');
+    const tableBody = document.getElementById('table-body');
+    
+    // ヘッダーをクリア
+    headerRow.innerHTML = '<th style="text-align: start;" id="top-left-cell" class="customtable_top-left-cell"></p></th>';
+    tableBody.innerHTML = '';
+
+    // ヘッダーにアイコンと値を追加
+    damageRange.forEach(value => {
+        const th = document.createElement('th');
+        th.classList.add('customtable_header');
+        const img = document.createElement('img');
+        img.src = 'damage+.png';
+        img.alt = 'damage+';
+        img.className = 'icon';
+        th.appendChild(img);
+        th.appendChild(document.createTextNode(` ${value}`));
+        headerRow.appendChild(th);
+    });
+    
+    // テーブルの行を生成
+    boostRange.forEach((value, row) => {
+        const tr = document.createElement('tr');
+        
+        // 行の見出しにアイコンと値を追加
+        const rowHeader = document.createElement('th');
+        const img = document.createElement('img');
+        rowHeader.classList.add('customtable_header');
+        rowHeader.classList.add('customtable_boost');
+        img.src = 'boost.png';
+        img.alt = 'boost';
+        img.className = 'icon';
+        rowHeader.appendChild(img);
+        rowHeader.appendChild(document.createTextNode(` ${value}`));
+        tr.appendChild(rowHeader);
+        
+        for (let col = 0; col < damageRange.length; col++) {
+            const td = document.createElement('td');
+            td.classList.add('customtable_cellcolor');
+            td.id = `cell${row}${col}`;
+            const damageValue = damageRange[col];
+            const boostValue = value;
+            const result_cell = (X + boostValue * A) * (1 + damageValue * 0.1 * A); // 計算式に基づく値
+            const result_cell2 = Math.max(Math.min(Math.ceil(result_cell), 999), 0);
+            let resultHTML = `<a title="${result_cell}">${result_cell2}</a>`;
+            
+            
+            if (((damageValue >= 0 && damageValue <= 2 && boostValue >= 0 && boostValue <= 2)&&!(damageValue == 0 && boostValue == 0))&&(result_cell2<999)) {
+                td.style.color = '#c0c0c0';
+            }
+            
+            if (result_cell2>=999){
+                //td.style.color = '#000000';
+                td.classList.remove('customtable_cellcolor');
+                td.style.opacity = '1';
+            }
+            td.innerHTML = resultHTML;
+            
+            td.addEventListener('mouseover', () => highlightCells(row, col));
+            td.addEventListener('mouseout', () => unhighlightCells(row, col));
+            tr.appendChild(td);
+        }
+        
+        tableBody.appendChild(tr);
+    });
+}
+
+function updateCellValue(row, col, value) {
+    const cellId = `cell${row}${col}`;
+    const cellElement = document.getElementById(cellId);
+    if (cellElement) {
+        cellElement.innerText = value;
+    }
+}
+
+function updateTopLeftCell(value) {
+    const topLeftCell = document.getElementById('top-left-cell');
+    topLeftCell.innerText = value;
+}
+
+function highlightCells(row, col) {
+    const headerRow = document.getElementById('header-row').children;
+    const tableBody = document.getElementById('table-body').children;
+
+    // 列のヘッダーをハイライト
+    headerRow[col + 1].classList.add('highlight');
+
+    // 行と列のセルをハイライト
+    for (let i = 0; i < tableBody.length; i++) {
+        tableBody[i].children[col + 1].classList.add('highlight'); // 列のセルをハイライト
+        tableBody[row].children[i + 1].classList.add('highlight'); // 行のセルをハイライト
+
+        // 行のヘッダーをハイライト
+        if (i === row) {
+            tableBody[i].children[0].classList.add('highlight');
+        }
+    }
+}
+
+function unhighlightCells(row, col) {
+    const headerRow = document.getElementById('header-row').children;
+    const tableBody = document.getElementById('table-body').children;
+
+    // 列のヘッダーのハイライトを解除
+    headerRow[col + 1].classList.remove('highlight');
+
+    // 行と列のセルのハイライトを解除
+    for (let i = 0; i < tableBody.length; i++) {
+        tableBody[i].children[col + 1].classList.remove('highlight'); // 列のセルのハイライトを解除
+        tableBody[row].children[i + 1].classList.remove('highlight'); // 行のセルのハイライトを解除
+
+        // 行のヘッダーのハイライトを解除
+        if (i === row) {
+            tableBody[i].children[0].classList.remove('highlight');
+        }
+    }
+}
+
+function updateTable(newX, newDamageStart, newBoostStart , newA) {
+    X = newX;
+    damageRange = Array.from({length: 10}, (_, i) => i + newDamageStart);
+    boostRange = Array.from({length: 10}, (_, i) => i + newBoostStart);
+    DamageStart = newDamageStart;
+    BoostStart = newBoostStart;
+    A = newA;
+    createTable(X,DamageStart,BoostStart,A);
+}
+
---
+++ docs/old/old_1_4_1/targetdamage_script.js
@@ -0,0 +1,325 @@
+let calculationType = false;
+
+function calculateTargetdamage() {
+
+    
+    let D = parseFloat(document.getElementById('targetdamage_input_basedamage').value);
+    let tD = parseFloat(document.getElementById('targetdamage_input_targetdamage').value);
+    let A = 1;
+            
+    
+    
+    let acustomflag  = document.querySelector('input[name="targetdamage_coeff"]:checked').id ==="targetdamage_targetdamage_acustom" ? true : false;
+            
+    if(acustomflag){
+        A = parseFloat(document.getElementById('targetdamage_acustominput').value);
+
+    }else{
+        A = parseFloat(document.querySelector('input[name="targetdamage_coeff"]:checked').value);
+        }
+    
+    //入力が不正なら0として扱う
+            
+            
+            if (isNaN(D)) {
+                D = 0;
+            }
+
+            if (isNaN(tD)) {
+                tD = 0;
+            }
+    
+            if (isNaN(A)) {
+                A = 1;
+            }
+    
+    const table = document.createElement('table');
+    const headerRow = document.createElement('tr');
+
+    let previousDamagePlus = null;
+    let previousBoost = null;
+    let foundtD = 0 ;
+    let col = 0 ;
+
+    if (!(calculationType)) {
+        const headerBoost = document.createElement('th');
+        headerBoost.innerHTML = '<img src="boost.png">'+messages[currentLanguage].targetdamage_table_boost;
+        headerRow.appendChild(headerBoost);
+
+        const headerDamagePlus = document.createElement('th');
+        headerDamagePlus.innerHTML = '<img src="damage+.png">'+messages[currentLanguage].targetdamage_table_damageplus;
+        headerRow.appendChild(headerDamagePlus);
+
+        const headerResultDamage = document.createElement('th');
+        headerResultDamage.textContent = messages[currentLanguage].targetdamage_table_damage;
+        headerRow.appendChild(headerResultDamage);
+
+        table.appendChild(headerRow);
+
+        for (let boost = 0; boost <= 99; boost++) {
+            let damagePlusFound = false;
+            for (let damagePlus = 0; damagePlus <= 99; damagePlus++) {
+                
+                const calculatedDamage_raw = ((D + boost * A) * (1 + damagePlus * 0.1 * A))
+                const calculatedDamage = Math.max(Math.min(Math.ceil(calculatedDamage_raw),999),0);
+            
+                
+
+                if ((calculatedDamage >= tD) || (((damagePlus == 0) && (boost == 0)))|| (((damagePlus == 99) && (boost == 99)))) {
+                    
+                    
+                    if (previousDamagePlus !== damagePlus) {
+                        
+                        if ((boost - 1 > previousBoost) && (foundtD>0)){
+                        //スキップした際、スキップしたのがわかるように
+                            
+                            
+                        const row = document.createElement('tr');
+
+                        const cellBoost = document.createElement('td');
+                        cellBoost.className += " targetdamage_skip";
+                        cellBoost.innerHTML = '︙';
+                        
+                        row.appendChild(cellBoost);
+
+                        const cellDamagePlus = document.createElement('td');
+                        cellDamagePlus.className += " targetdamage_skip";
+                        cellDamagePlus.innerHTML = '︙';
+                        row.appendChild(cellDamagePlus);
+
+                        const cellResultDamage = document.createElement('td');
+                        cellResultDamage.className += " targetdamage_skip";
+                        cellResultDamage.innerHTML = '︙';
+                        row.appendChild(cellResultDamage);
+
+                        table.appendChild(row);
+                        col += 1;
+                        
+                            
+                    }
+                        
+                        
+                        const row = document.createElement('tr');
+                        const currentcol = col;
+
+                        const cellBoost = document.createElement('td');
+                        cellBoost.innerHTML = '<img src="boost.png" > '+boost;
+                        row.appendChild(cellBoost);
+
+                        const cellDamagePlus = document.createElement('td');
+                        cellDamagePlus.innerHTML = '<img src="damage+.png" > '+damagePlus;
+                        row.appendChild(cellDamagePlus);
+
+                        const cellResultDamage = document.createElement('td');
+                        //cellResultDamage.textContent = calculatedDamage;
+                        cellResultDamage.innerHTML = `<a title="${calculatedDamage_raw}">${calculatedDamage}</a>`;
+                        row.appendChild(cellResultDamage);
+
+                        table.appendChild(row);
+                        previousDamagePlus = damagePlus;
+                        previousBoost = boost;
+                        
+                        
+                        
+                        //ハイライトなど
+                        const rowarray = [cellBoost , cellDamagePlus , cellResultDamage]
+                        
+                        for (const cellhighlight of rowarray) {
+                            cellhighlight.addEventListener('mouseover', () => targetdamage_highlightCells(currentcol) );
+                            cellhighlight.addEventListener('mouseout', () => targetdamage_unhighlightCells(currentcol));
+                            
+                            if (calculatedDamage < tD){
+                                cellhighlight.style.color = '#a0a0a0';
+                            }
+                            
+                            
+                            
+                            }
+                        
+                        
+                        
+                        col += 1;
+                        
+                        
+                    }
+                    
+                    if (calculatedDamage >= tD){
+                        foundtD += 1;
+                        damagePlusFound = true;
+                        break;
+                        }
+                    
+                    
+                }
+            }
+            if (!damagePlusFound) {
+                previousDamagePlus = null;
+                previousBoost = null;
+                
+            }
+        }
+    } else if (calculationType) {
+        const headerDamagePlus = document.createElement('th');
+        headerDamagePlus.innerHTML = '<img src="damage+.png">'+messages[currentLanguage].targetdamage_table_damageplus;
+        headerRow.appendChild(headerDamagePlus);
+
+        const headerBoost = document.createElement('th');
+        headerBoost.innerHTML = '<img src="boost.png">'+messages[currentLanguage].targetdamage_table_boost;
+        headerRow.appendChild(headerBoost);
+
+        const headerResultDamage = document.createElement('th');
+        headerResultDamage.textContent = messages[currentLanguage].targetdamage_table_damage;
+        headerRow.appendChild(headerResultDamage);
+
+        table.appendChild(headerRow);
+
+        for (let damagePlus = 0; damagePlus <= 99; damagePlus++) {
+            let boostFound = false;
+            for (let boost = 0; boost <= 99; boost++) {
+                
+                const calculatedDamage_raw = ((D + boost * A) * (1 + damagePlus * 0.1 * A))
+                const calculatedDamage = Math.max(Math.min(Math.ceil(calculatedDamage_raw),999),0);
+                
+                if ((calculatedDamage >= tD) || (((damagePlus == 0) && (boost == 0)))|| (((damagePlus == 99) && (boost == 99)))) {
+                    if (previousBoost !== boost) {
+                        
+                        if ((damagePlus - 1 > previousDamagePlus) && (foundtD>0)){
+                        //スキップした際、スキップしたのがわかるように
+                            
+                            
+                        const row = document.createElement('tr');
+
+                        const cellDamagePlus = document.createElement('td');
+                        cellDamagePlus.className += " targetdamage_skip";
+                        cellDamagePlus.innerHTML = '︙';
+                        
+                        row.appendChild(cellDamagePlus);
+
+                        const cellBoost = document.createElement('td');
+                        cellBoost.className += " targetdamage_skip";
+                        cellBoost.innerHTML = '︙';
+                        row.appendChild(cellBoost);
+
+                        const cellResultDamage = document.createElement('td');
+                        cellResultDamage.className += " targetdamage_skip";
+                        cellResultDamage.innerHTML = '︙';
+                        row.appendChild(cellResultDamage);
+
+                        table.appendChild(row);
+                        
+                        col += 1;
+                    }
+                        
+                        const row = document.createElement('tr');
+                        const currentcol = col;
+                        
+                        
+                        const cellDamagePlus = document.createElement('td');
+                        cellDamagePlus.innerHTML = '<img src="damage+.png" > '+damagePlus;
+                        row.appendChild(cellDamagePlus);
+
+                        const cellBoost = document.createElement('td');
+                        cellBoost.innerHTML = '<img src="boost.png" > '+boost;
+                        row.appendChild(cellBoost);
+
+                        const cellResultDamage = document.createElement('td');
+                        cellResultDamage.innerHTML = `<a title="${calculatedDamage_raw}">${calculatedDamage}</a>`;
+                        row.appendChild(cellResultDamage);
+
+                        table.appendChild(row);
+                        previousBoost = boost;
+                        previousDamagePlus = damagePlus;
+                        
+                        //ハイライトなど
+                        const rowarray = [cellBoost , cellDamagePlus , cellResultDamage]
+                        
+                        for (const cellhighlight of rowarray) {
+                            cellhighlight.addEventListener('mouseover', () => targetdamage_highlightCells(currentcol) );
+                            cellhighlight.addEventListener('mouseout', () => targetdamage_unhighlightCells(currentcol));
+                            
+                            if (calculatedDamage < tD){
+                                cellhighlight.style.color = '#a0a0a0';
+                            }
+                            
+                            
+                            }
+                        
+                        col += 1;
+                        
+                    }
+                    
+                    if (calculatedDamage >= tD){
+                        foundtD += 1;
+                        boostFound = true;
+                        }
+                    break;
+                }
+            }
+            if (!boostFound) {
+                previousBoost = null;
+                previousDamagePlus = null;
+            }
+        }
+    }
+
+    const resultTable = document.getElementById('resultTable');
+    resultTable.innerHTML = ''; // 前の結果をクリア
+    resultTable.appendChild(table);
+}
+
+
+function changeTargetdamage() {
+    calculationType = !calculationType;
+    calculateTargetdamage();
+}
+
+function targetdamage_highlightCells(col) {
+
+    const tableBody = document.getElementById('resultTable').children;
+
+    // 行と列のセルをハイライト
+    for (let i = 0; i < tableBody.length; i++) {
+        tableBody[i].children[col + 1].classList.add('targetdamage_highlight'); // 列のセルをハイライト
+
+    }
+}
+
+function targetdamage_unhighlightCells(col) {
+    
+    const tableBody = document.getElementById('resultTable').children;
+
+    // 行と列のセルのハイライトを解除
+    for (let i = 0; i < tableBody.length; i++) {
+        
+        tableBody[i].children[col + 1].classList.remove('targetdamage_highlight'); // 列のセルのハイライトを解除
+
+        
+    }
+}
+
+function targetdamage_clearInput() {
+            
+            document.getElementById('targetdamage_input_basedamage').value = "";
+            document.getElementById('targetdamage_acustominput').value = "";
+            document.getElementById('targetdamage_input_targetdamage').value = "";
+            document.getElementById('targetdamage_a2').checked=true;
+            calculateTargetdamage();
+            
+        }
+
+function targetdamage_setPreset(value) {
+            document.getElementById('targetdamage_input_basedamage').value = value;
+            calculateTargetdamage();
+        }
+
+
+
+function targetdamage_console(message){
+    console.log(message);
+}
+
+
+
+
+
+calculateTargetdamage();
